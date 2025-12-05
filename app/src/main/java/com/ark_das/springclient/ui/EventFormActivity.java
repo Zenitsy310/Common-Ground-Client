f78d007956ebdb4a8409eb8f68fde553a93f784b
Kokushibo <olegmark20000@gmail.com>
2025-12-05 11:12:31 +0300

    finished pikker data/time

--- app/src/main/java/com/ark_das/springclient/ui/EventFormActivity.java
+++ app/src/main/java/com/ark_das/springclient/ui/EventFormActivity.java
@@ -1,5 +1,12 @@
 package com.ark_das.springclient.ui;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +16,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.ark_das.springclient.R;
 
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class EventFormActivity extends AppCompatActivity {
+    TextView currentDateTime;
+    Calendar dateAndTime=Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +34,61 @@ public class EventFormActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        currentDateTime = findViewById(R.id.input_datetime);
+
+        setInitialDateTime();
     }
+
+    public void setDate(View v) {
+        new DatePickerDialog(EventFormActivity.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    // отображаем диалоговое окно для выбора времени
+    public void setTime(View v) {
+        new TimePickerDialog(EventFormActivity.this, t,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
+                .show();
+    }
+    // установка начальных даты и времени
+    private void setInitialDateTime() {
+        int day = dateAndTime.get(Calendar.DAY_OF_MONTH);
+        int month = dateAndTime.get(Calendar.MONTH) + 1;
+        int year = dateAndTime.get(Calendar.YEAR);
+        int hour = dateAndTime.get(Calendar.HOUR_OF_DAY);
+        int minute = dateAndTime.get(Calendar.MINUTE);
+
+        String formatted = String.format(Locale.getDefault(),
+                "%04d-%02d-%02d %02d:%02d",
+                year, month, day, hour, minute);
+
+        currentDateTime.setText(formatted);
+
+
+        //System.out.println(LocalDateTime.of());
+    }
+
+    // установка обработчика выбора времени
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+            setInitialDateTime();
+        }
+    };
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
 }
+
