2b9ec1cc5efd5c0297099a15b48ad4132d58a5ca
Kokushibo <olegmark20000@gmail.com>
2025-12-13 02:20:06 +0300

    create profile,settings,chat, layouts

---
+++ app/src/main/java/com/ark_das/springclient/ui/ChatListActivity.java
@@ -0,0 +1,25 @@
+package com.ark_das.springclient.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ark_das.springclient.R;
+
+public class ChatListActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_chat_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_chat_list), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
